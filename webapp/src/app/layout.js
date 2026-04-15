326bd4077ec1e72b76f98a344e68ac8b5974ab17
muhammadhassan191 <rizvihassanmuhammad@gmail.com>
2026-04-15 11:44:20 +0500

    user analysis (vercel)

--- webapp/src/app/layout.js
+++ webapp/src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = {
   title: 'Starstruck Cinemas Web',
@@ -61,7 +62,8 @@ export default function RootLayout({ children }) {
             }
           `
         }} />
-      </body>
+          <Analytics />
+    </body>
     </html>
   );
 }
